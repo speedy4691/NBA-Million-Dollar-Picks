309a35b302b88c43678bf43485dbba3c04b5d1c3
speedy4691 <cschober@tulane.edu>
2025-04-27 05:12:01 -0700

    Update NBA_Million_Dollar.py

--- NBA_Million_Dollar.py
+++ NBA_Million_Dollar.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 import pandas as pd
