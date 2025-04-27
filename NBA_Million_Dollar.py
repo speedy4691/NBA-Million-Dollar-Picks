f479aa1dad018c693c59549643a602f817569233
speedy4691 <cschober@tulane.edu>
2025-04-27 05:44:21 -0700

    Update NBA_Million_Dollar.py

--- NBA_Million_Dollar.py
+++ NBA_Million_Dollar.py
@@ -1,18 +1,24 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 import pandas as pd
 import streamlit as st
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.core.os_manager import ChromeType
 
-i=0
-j=0
+def get_driver():
+    return webdriver.Chrome(
+        service=Service(
+            ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()
+        ),
+        options=options,
+    )
 
-options = webdriver.ChromeOptions()
-options.add_argument('--headless')
-service = Service(ChromeDriverManager().install())
-driver = webdriver.Chrome(service=service, options=options)
-
+options = Options()
+options.add_argument("--disable-gpu")
+options.add_argument("--headless")
+driver = get_driver()
 
 
 driver.get('https://www.sportsline.com/nba/odds/')
