963302821a0eff3c7904f6997a7f464c0f4a0599
speedy4691 <cschober@tulane.edu>
2025-04-27 07:00:25 -0700

    Update NBA_Million_Dollar.py

--- NBA_Million_Dollar.py
+++ NBA_Million_Dollar.py
@@ -1,26 +1,18 @@
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.by import By
 import pandas as pd
 import streamlit as st
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
-from webdriver_manager.core.os_manager import ChromeType
-
-def get_driver():
-    return webdriver.Chrome(
-        service=Service(
-            ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()
-        ),
-        options=options,
-    )
+#from st_aggrid import AgGrid, GridOptionsBuilder, GridUpdateMode
 
 options = Options()
-options.add_argument("--disable-gpu")
-options.add_argument("--headless")
-driver = get_driver()
+options.headless = True
+i=0
+j=0
+#References the latest version of chromedriver to use 
+driver = webdriver.Firefox(options=options)
 
-
+#driver = webdriver.Chrome('C:\Python\Python310\Web Scrape\chromedriver.exe')
 driver.get('https://www.sportsline.com/nba/odds/')
 teams = driver.find_elements(By.XPATH, '//div[@data-testid="Team-name"]')
 teams = [element.text for element in teams]
@@ -28,7 +20,7 @@ odds = driver.find_elements(By.XPATH, '//span[@class="primary"]')
 odds = [element.text for element in odds]
 payout = driver.find_elements(By.XPATH, '//span[@class="secondary"]')
 payout = [element.text for element in payout]
-date = driver.find_elements(By.XPATH,"//div[@class='date']")
+date = driver.find_elements(By.XPATH,"//p[@class='sc-753a78ea-0 sc-753a78ea-1 hvAzLq emdlKR']")
 date = [element.text for element in date]
 # print(teams)
 # print(odds)
@@ -38,7 +30,7 @@ driver.quit()
 Money_Line=[]
 Line=[]
 Over_Under=[]
-for i in range(0,len(odds),3):
+for i in range(0,len(odds),6):
     a=odds[i]
     b=odds[i+1]
     c=odds[i+2]
@@ -49,7 +41,7 @@ game_date=[]
 for i in range(0,2*len(date),1):
     remainder=i%2
     if remainder==0:
-        date[j]=date[j][0:5]
+        date[j]=date[j][0:6]
         game_date.append(date[j])
     else:
         game_date.append(date[j])
@@ -62,15 +54,14 @@ for i in range(len(Over_Under)):
 # print(Line)
 # print(Over_Under)
 
-data={'Teams': teams, 'Date': game_date, 'Money Line': Money_Line, 'Line': Line, 'Over/Under': Over_Under}
-df = pd.DataFrame(data)
-#print(df)
 
-Chase=[]
-William=[]
-Fletcher=[]
-Cole=[]
+Bets=[]
+for i in range(len(Over_Under)):
+     Bets.append("no")
+
+
+data={'Teams': teams, 'Date': game_date, 'Money Line': Money_Line, 'Line': Line, 'Over/Under': Over_Under, "Chase": Bets, "Cole": Bets, "Fletch": Bets}
+df = pd.DataFrame(data)
 
 st.title('NBA Million Dollar Picks')
 st.dataframe(df)
-
