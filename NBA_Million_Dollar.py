79b8f1b5bde2001d360edd02a1cc72a9938c8463
speedy4691 <cschober@tulane.edu>
2025-04-27 05:30:57 -0700

    Update NBA_Million_Dollar.py

--- NBA_Million_Dollar.py
+++ NBA_Million_Dollar.py
@@ -5,13 +5,15 @@ from selenium.webdriver.common.by import By
 import pandas as pd
 import streamlit as st
 
+i=0
+j=0
+
+chromedriver_path = '/chromedriver'
+service = Service(chromedriver_path)
 options = webdriver.ChromeOptions()
 options.add_argument('--headless')
 
-i=0
-j=0
-#References the latest version of chromedriver to use 
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+driver = webdriver.Chrome(service=service, options=options)
 
 driver.get('https://www.sportsline.com/nba/odds/')
 teams = driver.find_elements(By.XPATH, '//div[@data-testid="Team-name"]')
