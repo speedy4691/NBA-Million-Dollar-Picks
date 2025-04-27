664a883aac7d0b33b54f945324ba9b1c731e8a86
speedy4691 <cschober@tulane.edu>
2025-04-27 09:20:32 -0700

    Update NBA_Million_Dollar.py

--- NBA_Million_Dollar.py
+++ NBA_Million_Dollar.py
@@ -18,7 +18,7 @@ def get_driver():
         service=Service(
             ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()
         ),
-        
+         options=options,
     )
 
 options = Options()
