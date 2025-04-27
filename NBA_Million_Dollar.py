5f08a598a9d2b9611c0ff6a42edd5af9097a5a84
speedy4691 <cschober@tulane.edu>
2025-04-27 05:11:06 -0700

    Update NBA_Million_Dollar.py

--- NBA_Million_Dollar.py
+++ NBA_Million_Dollar.py
@@ -1,15 +1,10 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 import pandas as pd
 import streamlit as st
 
-options = Options()
-options.add_argument('--headless')
-options.add_argument('--no-sandbox')
-options.add_argument('--disable-dev-shm-usage')
 i=0
 j=0
 #References the latest version of chromedriver to use 
