0b47566aa7f814ccdad61228f40ed537698e7083
speedy4691 <cschober@tulane.edu>
2025-04-27 04:32:36 -0700

    Add files via upload

---
+++ NBA_Million_Dollar.py
@@ -0,0 +1,70 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+import time
+import pandas as pd
+import streamlit as st
+
+
+options = webdriver.ChromeOptions()
+options.add_argument('--headless')
+i=0
+j=0
+#References the latest version of chromedriver to use 
+service = Service(executable_path='C:\Python\Python310\Chromedriver\chromedriver.exe') 
+driver = webdriver.Chrome(service=service)
+#driver = webdriver.Chrome('C:\Python\Python310\Web Scrape\chromedriver.exe')
+driver.get('https://www.sportsline.com/nba/odds/')
+teams = driver.find_elements(By.XPATH, '//div[@data-testid="Team-name"]')
+teams = [element.text for element in teams]
+odds = driver.find_elements(By.XPATH, '//span[@class="primary"]')
+odds = [element.text for element in odds]
+payout = driver.find_elements(By.XPATH, '//span[@class="secondary"]')
+payout = [element.text for element in payout]
+date = driver.find_elements(By.XPATH,"//div[@class='date']")
+date = [element.text for element in date]
+# print(teams)
+# print(odds)
+# print(payout)
+#
+driver.quit()
+Money_Line=[]
+Line=[]
+Over_Under=[]
+for i in range(0,len(odds),3):
+    a=odds[i]
+    b=odds[i+1]
+    c=odds[i+2]
+    Money_Line.append(a)
+    Line.append(b)
+    Over_Under.append(c)
+game_date=[]
+for i in range(0,2*len(date),1):
+    remainder=i%2
+    if remainder==0:
+        date[j]=date[j][0:5]
+        game_date.append(date[j])
+    else:
+        game_date.append(date[j])
+        j=j+1    
+for i in range(len(Over_Under)):
+    Over_Under[i]=Over_Under[i][1:]
+# print(teams)
+# print((game_date))
+# print(Money_Line)
+# print(Line)
+# print(Over_Under)
+
+data={'Teams': teams, 'Date': game_date, 'Money Line': Money_Line, 'Line': Line, 'Over/Under': Over_Under}
+df = pd.DataFrame(data)
+#print(df)
+
+Chase=[]
+William=[]
+Fletcher=[]
+Cole=[]
+
+st.title('NBA Million Dollar Picks')
+st.dataframe(df)
+
