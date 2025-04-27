6629d759d5d6d15980819acbeb321dbce32b8d56
speedy4691 <cschober@tulane.edu>
2025-04-27 07:34:24 -0700

    Update NBA_Million_Dollar.py

--- NBA_Million_Dollar.py
+++ NBA_Million_Dollar.py
@@ -1,16 +1,28 @@
 from selenium import webdriver
-from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.core.os_manager import ChromeType
 from selenium.webdriver.common.by import By
 import pandas as pd
 import streamlit as st
 #from st_aggrid import AgGrid, GridOptionsBuilder, GridUpdateMode
 
-options = Options()
-options.headless = True
-i=0
 j=0
-#References the latest version of chromedriver to use 
-driver = webdriver.Firefox(options=options)
+i=0
+def get_driver():
+    return webdriver.Chrome(
+        service=Service(
+            ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()
+        ),
+        options=options,
+    )
+
+options = Options()
+options.add_argument("--disable-gpu")
+options.add_argument("--headless")
+
+driver = get_driver()
 
 #driver = webdriver.Chrome('C:\Python\Python310\Web Scrape\chromedriver.exe')
 driver.get('https://www.sportsline.com/nba/odds/')
@@ -20,7 +32,7 @@ odds = driver.find_elements(By.XPATH, '//span[@class="primary"]')
 odds = [element.text for element in odds]
 payout = driver.find_elements(By.XPATH, '//span[@class="secondary"]')
 payout = [element.text for element in payout]
-date = driver.find_elements(By.XPATH,"//p[@class='sc-753a78ea-0 sc-753a78ea-1 hvAzLq emdlKR']")
+date = driver.find_elements(By.XPATH,"//div[@class='date']")
 date = [element.text for element in date]
 # print(teams)
 # print(odds)
@@ -30,7 +42,7 @@ driver.quit()
 Money_Line=[]
 Line=[]
 Over_Under=[]
-for i in range(0,len(odds),6):
+for i in range(0,len(odds),3):
     a=odds[i]
     b=odds[i+1]
     c=odds[i+2]
@@ -41,7 +53,7 @@ game_date=[]
 for i in range(0,2*len(date),1):
     remainder=i%2
     if remainder==0:
-        date[j]=date[j][0:6]
+        date[j]=date[j][0:5]
         game_date.append(date[j])
     else:
         game_date.append(date[j])
@@ -53,8 +65,6 @@ for i in range(len(Over_Under)):
 # print(Money_Line)
 # print(Line)
 # print(Over_Under)
-
-
 Bets=[]
 for i in range(len(Over_Under)):
      Bets.append("no")
